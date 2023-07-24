05c99dc267b659c3fe7e1fda627985b6af59b494
04burhanuddin <dev.burhanuddin@gmail.com>
2023-07-24 10:44:14 +0700

    protected route

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/redirects', [HomeController::class, 'index']);
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/redirects', [HomeController::class, 'index'])->name('dashboard');
 });
